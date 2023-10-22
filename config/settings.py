26e88912655b9edf117dba229f1f9fc5cd3e9408
sergei65105 <sergei65105.lat@mail.ru>
2023-10-22 16:49:08 +0300

    logichnoe\

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'playlist',
 ]
 
 MEDIA_URL = '/media/'
--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path
-from blog.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from playlist.views import *
+from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/posts_list/', posts_list),
     path('guess_numbers/', guess_numbers),
-    path('create/', create)
+    path('create/', create),
+    path('playlist/', playlist),
 
 ]
 if settings.DEBUG:
